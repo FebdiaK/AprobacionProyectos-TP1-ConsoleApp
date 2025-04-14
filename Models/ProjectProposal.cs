436923809dc4acdeb1eb39c7de10388d27402f7a
FedericoBDiaz <fede_diaz30@hotmail.com>
2025-04-14 06:35:57 -0300

    Inicio de repositorio

---
+++ Models/ProjectProposal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprobacionProyectos.Models
+{
+    public class ProjectProposal
+    {
+        [Key]
+        public required Guid Id { get; set; }
+
+        [MaxLength(255)]
+        public string Title { get; set; } = null!;
+
+        [Column(TypeName = "varchar(max)")]
+        public string Description { get; set; } = null!;
+
+        [ForeignKey("Area")]
+        public int AreaId { get; set; } // Foreign key del Area
+        public Area Area { get; set; } = null!; // Relación con Area
+
+        [ForeignKey("Type")]
+        public int TypeId { get; set; } // Foreign key del ProyectType
+        public ProjectType Type { get; set; } = null!; // Relación con ProyectType
+
+        public decimal EstimatedAmount { get; set; }  // monto estimado del proyecto
+        public int EstimatedDuration { get; set; }  // en dias segun el tp
+
+        [ForeignKey("Status")]
+        public int StatusId { get; set; } // Foreign key del ApprovalStatus
+        public ApprovalStatus Status { get; set; } = null!; // Relación con ApprovalStatus
+
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("CreatedBy")]
+        public int CreatedById { get; set; } // Foreign key del User
+        public User CreatedBy { get; set; } = null!; // Relación con User
+
+        public ICollection<ProjectApprovalStep> ApprovalSteps { get; set; } = new List<ProjectApprovalStep>();
+
+    }
+}
