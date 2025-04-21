3f5b3ddf6e80d5769ad0fb3136fb93432af84e39
FedericoBDiaz <84342124+FebdiaK@users.noreply.github.com>
2025-04-21 10:17:53 -0300

    Revert "Todo funcionando como se debe por ahora. "

---
+++ Data/Seeders/AreaSeeder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AprobacionProyectos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AprobacionProyectos.Data.Seeders
+{
+    public static class AreaSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Area>().HasData(
+                new Area { Id = 1, Name = "Finanzas" },
+                new Area { Id = 2, Name = "Tecnología" },
+                new Area { Id = 3, Name = "Recursos Humanos" },
+                new Area { Id = 4, Name = "Operaciones" }
+            );
+        }
+    }
+}
